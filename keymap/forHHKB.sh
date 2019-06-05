dc0d2ec9170919d28d8cea16e66a2356827d4cb8
4afS <g2x649@gmail.com>
2019-06-05 17:13:24 +0900

    for HHKB

---
+++ keymap/forHHKB.sh
@@ -0,0 +1,2 @@
+sudo cp ../keymap/basic_pc /usr/share/X11/xkb/symbols/pc
+sudo cp ../keymap/basic_us /usr/share/X11/xkb/symbols/us
---
+++ keymap/forSurface.sh
@@ -0,0 +1,2 @@
+sudo cp ../keymap/pc /usr/share/X11/xkb/symbols/pc
+sudo cp ../keymap/us /usr/share/X11/xkb/symbols/us
