4b297f2d07c8474e71ae176ed3a367f4cbef0155
Hibiki <g2x649@gmail.com>
2019-08-16 09:04:40 +0900

    fix getting config files

--- setup/neovim.sh
+++ setup/neovim.sh
@@ -1,12 +1,17 @@
 # neovim
 sudo apt install software-properties-common
-sudo add-apt-repository ppa:neovim-ppa/nostable -y
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt update
 sudo apt install neovim -y
 pip3 install neovim
 
-# get confing files
 mkdir -p $HOME/.config/nvim/
-wget -O "$HOME/.config/nvim/init.vim" https://raw.githubusercontent.com/4afs/setting/master/config/init.vim
+
+# get confing files
+cd $HOME
+git clone https://github.com/4afs/setting
+cd setting
+sh put_config.sh
+
 echo "export XDG_CONFIG_HOME='\$HOME/.config'" >> ~/.bashrc
 nvim +PlugInstall +"q!" +"q!"
