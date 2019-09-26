f84dc0c4b6f68166d22dfc3c2f8242a56663119a
4afS <4afS.h1@gmail.com>
2019-09-26 05:32:30 +0000

    add to set up git configurations

--- setup/init.sh
+++ setup/init.sh
@@ -14,3 +14,5 @@ if [ ! -e $HOME/.netrc ]; then
   echo -e "machine github.com\nlogin \npassword" > $HOME/.netrc
 fi
 
+git config --global user.email "4afS.h1@gmail.com"
+git config --global user.name "4afS"
